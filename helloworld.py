7793ad53bafb48119b6d5a5c339e6eea08d244b9
Vasili Pupkin <vasya@pupkin.com>
2013-11-14 15:36:18 +0200

    Merge hw3 (blog) with hw4 (sessions)

--- helloworld.py
+++ helloworld.py
@@ -203,9 +203,41 @@ class LogoutHandler(AuthorizeHandler):
 	self.unauthorize()
 	self.redirect("/signup");
 
+class Post(db.Model):
+    subject = db.StringProperty(required = False)
+    content = db.TextProperty(required = False)
+    date = db.DateTimeProperty(auto_now_add = True)
+
+class ListHandler(BaseTemplateHandler):
+    def get(self):
+	self.render("list.html", posts=Post.gql("ORDER BY date DESC"))
+
+class AddHandler(BaseTemplateHandler):
+    def get(self):
+	self.render("newpost.html")
+    def post(self):
+	subject = self.request.get("subject") 
+	content = self.request.get("content")
+	if not subject or not content:
+	    self.render("newpost.html", subject=subject, content=content, error = "Fuck you!")
+	else:
+	    p = Post(subject=subject, content=content)
+	    p.put()
+	    self.redirect("/show/%s" % p.key().id())
+
+class ShowHandler(BaseTemplateHandler):
+    def get(self, post_id):
+	p = Post.get_by_id(int(post_id))
+	if not p:
+	    self.redirect("/")
+	self.render("show.html", post=p)
+
 application = webapp2.WSGIApplication([
     ('/welcome', WelcomeHandler),
     ('/signup', SignupHandler),
     ('/login', LoginHandler),
     ('/logout', LogoutHandler),
+    ('/', ListHandler),
+    ('/newpost', AddHandler),
+    ('/show/(\d+)', ShowHandler),
     ], debug=True)
